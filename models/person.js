bf969ee7f153a04249596c92dac9d718c5bb3e64
heimdahl30 <67910058+heimdahl30@users.noreply.github.com>
2025-06-30 19:21:24 +0530

    Add files via upload

--- models/person.js
+++ models/person.js
@@ -2,14 +2,14 @@ const mongoose = require('mongoose')
 
 mongoose.set('strictQuery', false)
 
-const assert = require('assert');
+const assert = require('assert')
 
 const url = process.env.MONGO_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
 
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch(error => {
@@ -19,18 +19,18 @@ mongoose.connect(url)
 const personSchema = new mongoose.Schema({
   name: {
     type: String,
-    minlength: [3, 'Name must be at least 3 characters long']  
+    minlength: [3, 'Name must be at least 3 characters long']
   },
   number: {
     type: String,
     validate: {
       validator: function (v){
-        return /^\d{2,3}-\d+/.test(v);
+        return /^\d{2,3}-\d+/.test(v)
       },
-      message: ({value}) => `${value} is not a valid phone number`
+      message: ({ value }) => `${value} is not a valid phone number`
     },
     required: [true, 'Phone number required']
-    }
+  }
 })
 
 const Person = mongoose.model('Person', personSchema)
@@ -43,11 +43,11 @@ personSchema.set('toJSON', {
   }
 })
 
-let error;
+let error
 
 const tooShortName = new Person({
-name: "Jo",
-number: "123-45678"
+  name: 'Jo',
+  number: '123-45678'
 })
 
 error = tooShortName.validateSync()
@@ -55,40 +55,40 @@ assert.equal(error.errors['name'].message, 'Name must be at least 3 characters l
 
 
 const wrongNumber = new Person({
-name: "Joan",
-number: "1-45678"
+  name: 'Joan',
+  number: '1-45678'
 })
 
 error = wrongNumber.validateSync()
 assert.equal(error.errors['number'].message, `${wrongNumber.number} is not a valid phone number`)
 
 const wrongNumber1 = new Person({
-name: "Joan",
-number: "12345678"
+  name: 'Joan',
+  number: '12345678'
 })
 
 error = wrongNumber1.validateSync()
 assert.equal(error.errors['number'].message, `${wrongNumber1.number} is not a valid phone number`)
 
 const wrongNumber2 = new Person({
-name: "Joan",
-number: ''
+  name: 'Joan',
+  number: ''
 })
 
 error = wrongNumber2.validateSync()
-assert.equal(error.errors['number'].message, `Phone number required`)
+assert.equal(error.errors['number'].message, 'Phone number required')
 
 const wrongNumber3 = new Person({
-name: "Joan",
-number: "a12-123455"
+  name: 'Joan',
+  number: 'a12-123455'
 })
 
 error = wrongNumber3.validateSync()
 assert.equal(error.errors['number'].message, `${wrongNumber3.number} is not a valid phone number`)
 
 const wrongNumber4 = new Person({
-name: "Joan",
-number: "12-abc"
+  name: 'Joan',
+  number: '12-abc'
 })
 
 error = wrongNumber4.validateSync()
