2a4989840d2111c3fe917984165966446d75b993
heimdahl30 <67910058+heimdahl30@users.noreply.github.com>
2025-07-01 14:29:35 +0530

    Update mongo.js

--- mongo.js
+++ mongo.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const mongoURI = process.env.MONGO_URI
 
 mongoose.connect(mongoURI)
-  .then(() => console.log('MongoDB connected'))
+  .then(result => console.log('MongoDB connected'))
   .catch(err => console.log(err))
 
 module.exports = mongoose
